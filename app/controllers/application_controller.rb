e5ca0a8d8ee3fe1769958e889ffa345f81b3c914
Shea Kusiak <shkusiak@gmail.com>
2018-07-31 23:43:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
     if user.save
       redirect '/login'
     else
-      redirect '/failure'
+      redirect "/failure"
     end
   end
 
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
 			session[:user_id] = user.id
 			redirect '/success'
 		else
-			redirect '/failure'
+			redirect "/failure"
 		end
   end
 
